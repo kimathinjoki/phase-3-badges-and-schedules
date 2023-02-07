cfd89564658198aa5f2812389aa28cc471287eab
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-07 08:04:13 -0800

    Completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(arr)
+    arr.map do |b|
+        "Hello, my name is #{b}."
+    end
+end 
+
+
+def assign_rooms(list)
+
+    list.map.with_index(1) do |b,index| 
+        "Hello, #{b}! You'll be assigned to room #{index}!"
+    end
+
+end
+
+
+def printer(ar)
+    batch_badge_creator(ar).each do |b|
+        puts b
+    end
+
+    # puts assign_rooms(ar)
+    assign_rooms(ar).each do |b|
+        puts b
+    end
+
+
+end
+
+
+printer(["John","Cathy"])
